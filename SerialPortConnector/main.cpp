f9a198bc3e7e10bd8f4bacdb100d303caffdbea1
Adrian Miriuta <adrian.miriuta@gmail.com>
2020-05-28 12:20:58 +0200

    cleanup

--- SerialPortConnector/main.cpp
+++ SerialPortConnector/main.cpp
@@ -3,15 +3,11 @@
 #include <QApplication>
 #include <QStyleFactory>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QApplication a(argc, argv);
 
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
-    QApplication a(argc, argv);
-
-  //  a.setStyle(QStyleFactory::create("Fusion"));
-    Widget w;
-    w.show();
-    return a.exec();
+  Widget w;
+  w.show();
+  return (a.exec());
 }
